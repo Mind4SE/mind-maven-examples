0e16ef7b779fa4bc5ccb3a1f890687bdf4366776
Seyvoz Stéphane <stephane.seyvoz@non.schneider-electric.com>
2014-06-20 15:05:30 +0200

    Initial Maven Examples working commit, with basic MindEd setup.

---
+++ exampleApp/src/main/mind/main.c
@@ -0,0 +1,13 @@
+// -----------------------------------------------------------------------------
+// Implementation of the 'main' boot interface.
+// -----------------------------------------------------------------------------
+#include <stdio.h>
+
+//int main(int argc, char** argv)
+int METH(entryPoint, main) (int argc, char *argv[])
+{
+
+  printf("Hello, maybe you want to start adding some components now? :-) \n");
+  
+  return 0;
+}
